233e12c9b1c970d459b8b764dc5cb623af9a15eb
Smriti-sahu <sahusmriti147@gmail.com>
2024-01-22 02:00:12 +0530

    smritiSahu

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiPython,
+  DiGit,
+  DiJava,
+  DiHtml5,
+  DiCss3,
+} from "react-icons/di";
+import {
+  SiRedis,
+  SiFirebase,
+  SiNextdotjs,
+  SiSolidity,
+  SiPostgresql,
+  SiTailwindcss,
+} from "react-icons/si";
+// import { PiFileSqlLight } from "react-icons/pi";
+import { BsBootstrap ,  BsFiletypeSql} from "react-icons/bs";
+import { TbBrandGolang  ,TbFileTypeSql } from "react-icons/tb";
+
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJava />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <CgCPlusPlus />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <BsBootstrap />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <BsFiletypeSql />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTailwindcss />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <DiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+      <DiCss3 />
+      </Col>
+      
+    </Row>
+  );
+}
+
+export default Techstack;
---
+++ src/components/Footer.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3>Designed and Developed by Smriti Sahu</h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year}-Smriti sahu</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                  href="https://github.com/Smriti-sahu"
+                  style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            {/* <li className="social-icons">
+              <a
+                href="https://twitter.com/Soumyajit4419"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiOutlineTwitter />
+              </a>
+            </li> */}
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/smriti-sahu-b794591ba"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/smriti_.sahu?igsh=MWNmY3QyaTF2bWY1Nw=="
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
